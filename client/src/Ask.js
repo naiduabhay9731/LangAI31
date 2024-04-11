1e5a8d1bb5a7a8d03c4a79e74613478a761dedd0
Abhay Naidu <95175725+naiduabhay9731@users.noreply.github.com>
2024-04-11 23:31:20 +0530

    Update Ask.js

--- client/src/Ask.js
+++ client/src/Ask.js
@@ -44,7 +44,7 @@ const QuestionAnswer = () => {
       setData(data2);
     }
     try {
-      const response = await axios.post("http://localhost:5000/", {
+      const response = await axios.post("https://langai31.onrender.com/", {
         textData: data,
       });
       console.log(response);
